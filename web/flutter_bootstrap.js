0c00ca5bd0cbffe101cf106896719d1afd1eeb5a
Rajesh <hello@getreadyjob.com>
2026-07-17 15:02:57 +0530

    auto: sync site updates (2026-07-17 15:02:57)

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,9 +2,6 @@
 {{flutter_build_config}}
 
 _flutter.loader.load({
-	config: {
-		renderer: "html",
-	},
 	onEntrypointLoaded: async function (engineInitializer) {
 		const appRunner = await engineInitializer.initializeEngine();
 		await appRunner.runApp();
