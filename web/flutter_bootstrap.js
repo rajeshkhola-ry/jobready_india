bd11484eab92b507cd0678b9f8687b799b50b82a
Rajesh <hello@getreadyjob.com>
2026-07-17 16:57:19 +0530

    auto: sync site updates (2026-07-17 16:57:19)

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -4,6 +4,7 @@
 _flutter.loader.load({
 	config: {
 		canvasKitVariant: "full",
+		canvasKitForceCpuOnly: true,
 	},
 	onEntrypointLoaded: async function (engineInitializer) {
 		const appRunner = await engineInitializer.initializeEngine();
