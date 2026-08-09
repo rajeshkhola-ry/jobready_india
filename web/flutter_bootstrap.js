e4e1fac7c649b729476a756ff3f0b46b129674c3
Rajesh <hello@getreadyjob.com>
2026-08-09 14:46:06 +0530

    fix(web): version Flutter entrypoint cache

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,12 +1,24 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load({
-  onEntrypointLoaded: async function (engineInitializer) {
-    const appRunner = await engineInitializer.initializeEngine({
-      renderer: 'canvaskit',
-      useColorEmoji: false,
-    });
-    await appRunner.runApp();
-  },
-});
+(function () {
+  var entrypointVersion = 'grj-voice-card-20260809';
+  var builds = (_flutter.buildConfig && _flutter.buildConfig.builds) || [];
+
+  builds.forEach(function (build) {
+    if (build.mainJsPath) {
+      var separator = build.mainJsPath.indexOf('?') === -1 ? '?' : '&';
+      build.mainJsPath = build.mainJsPath + separator + 'v=' + entrypointVersion;
+    }
+  });
+
+  _flutter.loader.load({
+    onEntrypointLoaded: async function (engineInitializer) {
+      var appRunner = await engineInitializer.initializeEngine({
+        renderer: 'canvaskit',
+        useColorEmoji: false,
+      });
+      await appRunner.runApp();
+    },
+  });
+})();
